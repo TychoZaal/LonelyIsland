5e6f8f3e2717e5359fbb8f43b4519ea238941433
TychoLaptop\iMrBa_000 <TychoZaal@gmail.com>
2019-04-07 14:08:17 +0200

    Sunday progress

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField]
+    private float speedVertical = 1f;
+    [SerializeField]
+    private float speedHorizontal = 1f;
+    public int speed = 5;
+    private CursorLockMode cursorState;
+    public float yaw = 77.819f;
+    public float pitch = 0.0f;
+    private float cameraHeight = 0.0f;
+
+    private void Start()
+    {
+        cursorState = CursorLockMode.Locked;
+        SetCursorState();
+    }
+
+    void FixedUpdate()
+    {
+        yaw += speedHorizontal * Input.GetAxis("Mouse X");
+        pitch -= speedVertical * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movement = transform.TransformDirection(movement);
+        movement *= speed;
+
+        transform.position += movement * speed;
+
+        // Unlock camera
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cursorState = CursorLockMode.None;
+            SetCursorState();
+        }
+
+        // Lock camera
+        if (Input.GetMouseButtonDown(0))
+        {
+            cursorState = CursorLockMode.Locked;
+            SetCursorState();
+        }
+    }
+
+    void SetCursorState()
+    {
+        Cursor.lockState = cursorState;
+        Cursor.visible = (CursorLockMode.Locked != cursorState);
+    }
+}
---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    public GameObject objectToSpawn, holderObject;
+    public float offsetXMin, offsetXMax, offsetYMin, offsetYMax, offsetZMin, offsetZMax;
+    public int objectsToSpawn = 5, minSize = 1, maxSize = 2;
+
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    void SpawnObject()
+    {
+        for (int i = 0; i < objectsToSpawn; i++)
+        {
+            GameObject objectSpawned = Instantiate(objectToSpawn, new Vector3(Random.Range(offsetXMin, offsetXMax), Random.Range(offsetYMin, offsetYMax), Random.Range(offsetZMin, offsetZMax)), Quaternion.identity, holderObject.transform );
+            float scale = Random.Range(minSize, maxSize);
+            objectSpawned.transform.localScale = new Vector3(scale, scale, scale);
+        }
+    }
+}
