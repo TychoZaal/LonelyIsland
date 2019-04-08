0f9798a8f88a46f1732683e3f3f70d1433c3b667
TychoDesktop <TychoZaal@gmail.com>
2019-04-08 17:38:00 +0200

    Palm trees

--- Assets/Scripts/MovementBubble.cs
+++ Assets/Scripts/MovementBubble.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovementBubble : MonoBehaviour
 {
-    public float speed;
+    public float speed, strength;
     public SpawnObjects spawnBubbles;
 
     private void Start()
@@ -13,6 +13,7 @@ public class MovementBubble : MonoBehaviour
         speed = Random.Range(speed - 0.3f, speed + 0.3f);
         if (speed < 0.2f)
             speed = 0.2f;
+        strength = speed + 5;
     }
 
     // Update is called once per frame
@@ -23,7 +24,7 @@ public class MovementBubble : MonoBehaviour
             transform.position = new Vector3(transform.position.x, Random.Range(spawnBubbles.offsetYMin, spawnBubbles.offsetYMax), transform.position.z);
         }
 
-        transform.position += (Mathf.Sin(2 * Mathf.PI * speed * Time.time) - Mathf.Sin(2 * Mathf.PI * speed * (Time.time - Time.deltaTime))) * transform.right;
+        transform.position += (Mathf.Sin(2 * Mathf.PI * speed * Time.time) - Mathf.Sin(2 * Mathf.PI * speed * (Time.time - Time.deltaTime))) * transform.right * strength;
         transform.position += new Vector3(0, speed / 2, 0);
     }
 }
